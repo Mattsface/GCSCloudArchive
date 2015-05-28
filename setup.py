4566aeeb593c95055c03aa8365f95774663f9125
mspah <mspah@zulily.com>
2015-05-28 16:25:43 -0700

    Created the skeleton for the project

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(name='GCSCloudArchive',
+      version='0.1.0',
+      description='New project to replace the current zuGCSCloudArchive.sh shell script for Google bucket storage transfers.',
+      author='Matthew Spah',
+      author_email='spahmatther@gmail.com',
+      scripts=[''],
+      packages=find_packages(),
+      install_requires=[
+          'nose'
+      ],
+      zip_safe=False)
+
+
